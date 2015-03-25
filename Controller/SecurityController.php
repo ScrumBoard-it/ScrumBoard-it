529f534c4d0a60cc6c91af24a1ae05a373b8f73b
unknown <sblaise@PAR-LAP89.canaltp.local>
2015-03-25 17:15:59 +0100

    add class to connect with form

---
+++ Controller/SecurityController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace CanalTP\ScrumBoardItBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
+
+class SecurityController extends Controller
+{
+
+    public function loginAction(Request $request)
+    {
+       // Si le visiteur est déjà identifié, on le redirige vers l'accueil
+    if ($this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+      return $this->redirect($this->generateUrl('canal_tp_postit_homepage'));
+    }
+
+    $session = $request->getSession();
+
+    // On vérifie s'il y a des erreurs d'une précédente soumission du formulaire
+    if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+      $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+    } else {
+      $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+      $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+    }
+
+    return $this->render('CanalTPScrumBoardItBundle:Security:login.html.twig', array(
+      // Valeur du précédent nom d'utilisateur entré par l'internaute
+      'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+      'error'         => $error,
+    ));
+    }
+
+    
+}
