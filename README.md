d4a1b46b09770764bcf5298063bd51ccfa4a3c21
brieucpouliquen <brieuc.pouliquen@canaltp.fr>
2016-04-13 17:13:21 +0200

    Migrate bundle to full app

--- README.md
+++ README.md
@@ -1,16 +1,4 @@
-ScrumBoardItBundle
-=============
+SBI
+===
 
-A tool to facilitate Scrum card printing on sticky notes from Atlassian Jira
-
-Documentation
--------------
-
-The documentation is stored in the `Resources/doc/index.rst` file in this bundle:
-
-[Read the Documentation](https://github.com/CanalTP/ScrumBoard-it/blob/master/Resources/doc/index.rst)
-
-Installation
-------------
-
-All the installation instructions are located in  the documentation.
+A Symfony project created on April 5, 2016, 12:44 pm.
---
+++ src/CanalTP/ScrumBoardItBundle/DependencyInjection/Security/Factory/JiraFactory.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace CanalTP\ScrumBoardItBundle\DependencyInjection\Security\Factory;
+
+use Symfony\Bundle\SecurityBundle\DependencyInjection\Security\Factory\AbstractFactory;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\DefinitionDecorator;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * jirafactory contains the form parameters.
+ */
+class JiraFactory extends AbstractFactory
+{
+    public function __construct()
+    {
+        $this->addOption('username_parameter', '_username');
+        $this->addOption('password_parameter', '_password');
+        $this->addOption('intention', 'authenticate');
+        $this->addOption('post_only', true);
+    }
+
+    protected function createAuthProvider(ContainerBuilder $container, $id, $config, $userProviderId)
+    {
+        $provider = 'jira_auth.authentication_provider.'.$id;
+        $container
+            ->setDefinition($provider, new DefinitionDecorator('jira_auth.authentication_provider'))
+            ->replaceArgument(0, new Reference($userProviderId))
+            ->replaceArgument(1, $id)
+        ;
+
+        return $provider;
+    }
+
+    protected function getListenerId()
+    {
+        return 'canaltp_jira_auth.authentication_listener';
+    }
+
+    public function getPosition()
+    {
+        return 'form';
+    }
+
+    public function getKey()
+    {
+        return 'jira';
+    }
+
+    protected function createListener($container, $id, $config, $userProvider)
+    {
+        $listenerId = parent::createListener($container, $id, $config, $userProvider);
+
+        if (isset($config['csrf_provider'])) {
+            $container
+                ->getDefinition($listenerId)
+                ->addArgument(new Reference($config['csrf_provider']))
+            ;
+        }
+
+        return $listenerId;
+    }
+
+    protected function createEntryPoint($container, $id, $config, $defaultEntryPoint)
+    {
+        $entryPointId = 'security.authentication.form_entry_point.'.$id;
+        $container
+            ->setDefinition($entryPointId, new DefinitionDecorator('security.authentication.form_entry_point'))
+            ->addArgument(new Reference('security.http_utils'))
+            ->addArgument($config['login_path'])
+            ->addArgument($config['use_forward'])
+        ;
+
+        return $entryPointId;
+    }
+}
