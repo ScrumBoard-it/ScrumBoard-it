4530ec82eeee2876d44a9b763f7bd6e19c410223
brieucpouliquen <brieuc.pouliquen@canaltp.fr>
2016-04-19 18:39:06 +0200

    19/04/2016 Security with Guard OK

--- src/ScrumBoardItBundle/Entity/User.php
+++ src/ScrumBoardItBundle/Entity/User.php
@@ -11,7 +11,6 @@ use Symfony\Component\Security\Core\User\EquatableInterface;
 class User implements UserInterface, EquatableInterface
 {
     private $username;
-    private $password;
     private $email;
     private $salt;
     private $roles;
@@ -25,34 +24,29 @@ class User implements UserInterface, EquatableInterface
     public function eraseCredentials()
     {
     }
-    public function getPassword()
-    {
-        return $this->password;
-    }
-    public function setPassword($password)
-    {
-        $this->password = $password;
-
-        return $this;
-    }
+    
     public function getRoles()
     {
         return $this->roles;
     }
+    
     public function setRoles($roles)
     {
         $this->roles = $roles;
     }
+    
     public function getSalt()
     {
         return $this->salt;
     }
+    
     public function setSalt($salt)
     {
         $this->salt = $salt;
 
         return $this;
     }
+    
     public function getUsername()
     {
         return $this->username;
@@ -75,22 +69,17 @@ class User implements UserInterface, EquatableInterface
         return $this;
     }
     
-    public function getBase64Hash()
-    {
-        return $this->hash;
+    public function setHash($hash) {
+        $this->hash = $hash;
     }
-    public function setBase64Hash($data)
-    {
-        $this->hash = base64_encode($data);
+    
+    public function getHash() {
+        return $this->hash;
     }
     
     public function isEqualTo(UserInterface $user)
     {
         if (!$user instanceof User) {
-            return false;
-        }
-
-        if ($this->password !== $user->getPassword()) {
             return false;
         }
 
@@ -101,7 +90,16 @@ class User implements UserInterface, EquatableInterface
         if ($this->username !== $user->getUsername()) {
             return false;
         }
+        
+        if ($this->hash !== $user->getHash()) {
+            return false;
+        }
 
         return true;
     }
+
+    public function getPassword() {
+        
+    }
+
 }
