fd26f8c61b9ae182e56b513c5b71eb4b7f01a9b4
brieucpouliquen <brieuc.pouliquen@canaltp.fr>
2016-04-19 18:39:06 +0200

    19/04/2016 Security with Guard OK

--- src/ScrumBoardItBundle/Controller/DefaultController.php
+++ src/ScrumBoardItBundle/Controller/DefaultController.php
@@ -7,7 +7,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security as Secure;
-use Symfony\Component\Security\Core\Security;
 
 /**
  * controller of navigation.
@@ -16,54 +15,22 @@ class DefaultController extends Controller {
 
     /**
      * @Route("/", name="index")
-     * 
+     * @Route("/logout", name="logout")    * 
      * @return Response
      */
     public function indexAction() {
-        //A changer : redirection automatique à faire
-        return new Response("<html><body>index</body></html>");
-    }
-
-    /**
-     * @Route("/login", name="login")
-     * 
-     * @param Request $request
-     * @return Response
-     */
-    public function loginAction(Request $request) {
-        $session = $request->getSession();
-        $error = '';
-        
-        if ($request->attributes->has(Security::AUTHENTICATION_ERROR)) {
-            $error = $request->attributes->get(Security::AUTHENTICATION_ERROR);
-        }
-
-        return $this->render('ScrumBoardItBundle:Security:login.html.twig', array(
-                    'last_username' => $session->get(Security::LAST_USERNAME),
-                    'error' => $error,
-        ));
+        return $this->redirect($this->generateUrl('login_check'));
     }
     
     /**
-     * @Route("/logout", name="logout")
+     * @Route("/home", name="home")
+     * @Secure("has_role('ROLE_AUTHENTICATED')")
      */
-    public function logoutAction() {
+    public function home() {
+        //ENTREE DANS L'APPLICATION
         
-    }
-    
-    /**
-     * @Route("/login_check", name="login_check")
-     * 
-     * @return type
-     */
-    public function loginCheckAction() {
-        dump($this->get('security.authorization_checker')->isGranted('ROLE_AUTHENTICATED'));
-       if ($this->get('security.authorization_checker')->isGranted('ROLE_AUTHENTICATED')) {
-            return $this->redirect($this->generateUrl('home'));
-        }
-        else {
-            return $this->redirect($this->generateUrl('login'));
-        }
+        return new Response('<html><head><title>Authentification | ScrumBoard-it</title></head><body>Bienvenue ' .
+                $this->getUser()->getUsername() . ' !</body></html>');
     }
 
     /**
@@ -86,15 +53,7 @@ class DefaultController extends Controller {
     }
 
     /**
-     * @Route("/home", name="home")
-     * @Secure("has_role('ROLE_AUTHENTICATED')")
-     */
-    public function home() {
-        return new Response("<html><body>Connecté</body></html>");
-    }
-
-    /**
-     * @Route("/flag/add", name="canal_tp_postit_add_flag")
+     * @Route("/flag/add", name="add_flag")
      * 
      * @param Request $request
      * @return Response
