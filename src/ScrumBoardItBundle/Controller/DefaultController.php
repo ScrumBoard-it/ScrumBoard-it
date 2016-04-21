6812b80804e236f33ff0b34e176fcf99d405b47a
brieucpouliquen <brieuc.pouliquen@canaltp.fr>
2016-04-21 16:19:15 +0200

    Debug dépoiement

--- src/ScrumBoardItBundle/Controller/DefaultController.php
+++ src/ScrumBoardItBundle/Controller/DefaultController.php
@@ -30,7 +30,6 @@ class DefaultController extends Controller {
     public function home() {
         //ENTREE DANS L'APPLICATION
         $user = $this->getUser();
-        dump($user);
         return new Response('<html><head><title>Bienvenue | ScrumBoard-it</title></head><body>Bienvenue ' .
                 $user->getDisplayName() . ' !<br/></body></html>');
     }
