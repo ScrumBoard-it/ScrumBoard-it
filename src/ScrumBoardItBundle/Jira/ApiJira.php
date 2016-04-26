d58e47bd6cd0b2b2016dec804bd5b6d06518c9bf
brieucpouliquen <brieuc.pouliquen@canaltp.fr>
2016-04-26 16:58:13 +0200

    Configuration APIs

---
+++ src/ScrumBoardItBundle/Jira/ApiJira.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace ScrumBoardItBundle\Jira;
+
+use ScrumBoardItBundle\Jira\AbstractApi;
+use ScrumBoardItBundle\Entity\Project\Project;
+use ScrumBoardItBundle\Entity\Sprint;
+use ScrumBoardItBundle\Entity\Issue\SubTask;
+use ScrumBoardItBundle\Entity\Issue\Task;
+
+class ApiJira extends AbstractApi {
+
+    public function getIssues($var) {
+        $url = $this->getData()['host'] . $this->getData()['api'] . 'search?jql=sprint=' . $var;
+        $data = json_decode($this->curl($url), true);
+        $issues = array();
+        for ($i = 0; $i < $data['total']; $i++) {
+            if ($data['issues'][$i]['fields']['issuetype']['subtask'] === true) {
+                $issue = new SubTask();
+                //Référencer la tâche parente
+            } else {
+                $issue = new Task();
+                $issue->setComplexity($data['issues'][$i]['fields']['customfield_11108']);
+            }
+            $issue->setId($data['issues'][$i]['id']);
+            $issue->setProject($data['issues'][$i]['key']);
+            $issue->setTitle($data['issues'][$i]['fields']['summary']);
+            if (!empty($data['issues'][$i]['fields']['labels']) && $data['issues'][$i]['fields']['labels'][0] === "Post-it") {
+                $issue->setPrinted(true);
+            } else {
+                $issue->setPrinted(false);
+            }
+            array_push($issues, $issue);
+        }
+        
+        return $issues;
+    }
+
+    public function getProjects() {
+        $url = $this->getData()['host'] . $this->getData()['agile'] . 'board?maxResults=-1';
+        $data = json_decode($this->curl($url), true);
+        $boards = array();
+        for ($i = 0; $i < count($data['values']); $i++) {
+            $board = new Project($data['values'][$i]['id'], $data['values'][$i]['name']);
+            array_push($boards, $board);
+        }
+
+        return $boards;
+    }
+
+    public function getSprints($var) {
+        $url = $this->getData()['host'] . $this->getData()['agile'] . 'board/' . $var . '/sprint?state=active&state=future';
+        $data = json_decode($this->curl($url), true);
+        $sprints = array();
+        for ($i = 0; $i < count($data['values']); $i++) {
+            $sprint = new Sprint($data['values'][$i]['id'], $data['values'][$i]['name']);
+            array_push($sprints, $sprint);
+        }
+
+        return $sprints;
+    }
+}
---
+++ src/ScrumBoardItBundle/Security/JiraAuthenticator.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ScrumBoardItBundle\Security;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+use ScrumBoardItBundle\Security\AbstractTokenAuthenticator;
+
+class JiraAuthenticator extends AbstractTokenAuthenticator {
+
+    public function checkCredentials($credentials, UserInterface $user) {
+        $login = $user->getUsername();
+        $password = $credentials['password'];
+        $user->setHash("$login:$password");
+        $url = $this->data['host'] . $this->data['api'] . 'user?username=' . $login;
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: Basic ' . $user->getHash()]);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+        $content = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        if ($httpCode == 200 && !empty($content)) {
+            $data = json_decode($content, true);
+            $user->setEmail($data['emailAddress']);
+            $user->setDisplayName($data['displayName']);
+            $user->setImgUrl($data['avatarUrls']['24x24']);
+            $user->setCoType('jira');
+            return true;
+        }
+        return false;
+    }
+}
