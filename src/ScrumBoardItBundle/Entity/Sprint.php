eb0bfffcf7d29cee94c5868c28bf4c707bd892ce
brieucpouliquen <brieuc.pouliquen@canaltp.fr>
2016-04-26 16:58:13 +0200

    Configuration APIs

---
+++ src/ScrumBoardItBundle/Entity/Sprint.php
@@ -0,0 +1,21 @@
+<?php
+namespace ScrumBoardItBundle\Entity;
+
+class Sprint {
+
+    private $id;
+    private $title;
+
+    public function __construct($id, $title) {
+        $this->id = $id;
+        $this->title = $title;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+    
+    public function getTitle() {
+        return $this->title;
+    }
+}
