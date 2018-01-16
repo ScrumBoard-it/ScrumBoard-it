c99c8b0bca9ef4247a89c17e3210f275fb119f9d
Johan Rouve <johan.rouve@gmail.com>
2018-01-16 17:33:33 +0100

    🎨 Add Redux to the App

--- app/src/App.js
+++ app/src/App.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import BoardList from './components/BoardList';
+import ConfigSteps from './container/ConfigSteps';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <BoardList />
-      </div>
+      <ConfigSteps />
     );
   }
 }
---
+++ app/src/actions.js
@@ -0,0 +1,5 @@
+export const SET_CONFIG = 'SET_CONFIG'
+
+export function setConfig(token) {
+  return { type: SET_CONFIG, token }
+}
