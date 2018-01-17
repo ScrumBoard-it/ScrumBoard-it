2b540475da8b99487e4716a7358d5bfa9e75f0e0
Thomas Noury <thomas.noury@kisio.org>
2018-01-17 16:39:51 +0100

    💄 Added material-ui lib

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
 import './App.css';
 
 import ConfigSteps from './container/ConfigSteps';
@@ -6,7 +9,11 @@ import ConfigSteps from './container/ConfigSteps';
 class App extends Component {
   render() {
     return (
-      <ConfigSteps />
+      <MuiThemeProvider>
+        <AppBar title="ScrumBoard-it" />
+        <RaisedButton label="Default" />
+        <ConfigSteps />
+      </MuiThemeProvider>
     );
   }
 }
